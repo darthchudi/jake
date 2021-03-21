0bf3312bd3ac67d324e901540e3d59f2c9442b18
darthchudi <chukwudioranu@ymail.com>
2021-03-21 22:13:21 +0100

    chore: mount graphiql playground

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
   get "/user", to: "user#index"
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
