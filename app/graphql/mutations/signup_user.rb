2d840288ca03b0662ef0888121b2ad224b4f3575
darthchudi <chukwudioranu@ymail.com>
2021-03-22 13:24:08 +0100

    chore: rename mutation

--- app/graphql/mutations/signup_user.rb
+++ app/graphql/mutations/signup_user.rb
@@ -1,5 +1,5 @@
 module Mutations 
-    class Signup < Mutations::BaseMutation
+    class SignupUser < Mutations::BaseMutation
         argument :email, String, required: true 
         argument :password, String, required: true 
         argument :city, String, required: true 
@@ -8,7 +8,7 @@ module Mutations
         field :token, String, null: false
 
         def resolve(email:, password:, city:)
-            result = SignupUser.call(email: email, password: password, city: city)
+            result = ::SignupUser.call(email: email, password: password, city: city)
             {user: result.user, token: result.token}
         end
     end 
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :signup, mutation: Mutations::Signup, description: "Creates a new user account"
+    field :signup, mutation: Mutations::SignupUser, description: "Creates a new user account"
   end
 end
