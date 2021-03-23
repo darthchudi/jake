8daed055f6a553401aad960cff4a73678f232c1a
darthchudi <chukwudioranu@ymail.com>
2021-03-23 21:20:00 +0100

    chore: declare mutations for daily weather updates

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
     field :signup, mutation: Mutations::SignupUser, description: "Creates a new user account"
+    field :login, mutation: Mutations::LoginUser, description: "Logs in a user and returns a token"
+    field :subscribe_weather_update, mutation: Mutations::SubscribeWeatherUpdate, description: "Subscribes a user to the daily weather update"
+    field :unsubscribe_weather_update, mutation: Mutations::UnsubscribeWeatherUpdate, description: "Unsubscribes a user from the daily weather update"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_171233) do
+ActiveRecord::Schema.define(version: 2021_03_23_195331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_171233) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
