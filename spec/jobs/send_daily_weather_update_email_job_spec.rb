6a4370c083b28bda2a09c620b3e0407436c557ba
darthchudi <chukwudioranu@ymail.com>
2021-03-23 20:43:32 +0100

    chore: add circleci config

--- spec/jobs/send_daily_weather_update_email_job_spec.rb
+++ spec/jobs/send_daily_weather_update_email_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SendDailyWeatherUpdateEmailJob, type: :job do
+RSpec.describe SendDailyWeatherUpdateEmailsJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
