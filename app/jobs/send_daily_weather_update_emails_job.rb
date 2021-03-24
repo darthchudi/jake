50f6294d94576b38ee75644ee34976dde872f0ba
darthchudi <chukwudioranu@ymail.com>
2021-03-24 21:03:13 +0100

    fix: use find_each to iterate through records

--- app/jobs/send_daily_weather_update_emails_job.rb
+++ app/jobs/send_daily_weather_update_emails_job.rb
@@ -1,6 +1,6 @@
 class SendDailyWeatherUpdateEmailsJob < ApplicationJob
   def perform
-    User.where(is_subscribed: true).each do |user|
+    User.where(is_subscribed: true).find_each do |user|
       SendEmailJob.perform_later(user)
     end 
   end
