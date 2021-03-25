ab42e4337f91c250453f9b3ce9f42631d3083062
darthchudi <chukwudioranu@ymail.com>
2021-03-25 16:05:09 +0100

    feat: add interactor tests

--- spec/interactors/get_weather_spec.rb
+++ spec/interactors/get_weather_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe GetWeather, type: :interactor do
-  describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  subject(:context) do 
+    described_class.call(city: city) 
+  end 
+  
+  let (:city) {["madrid", "london", "lagos", "barcelona"].sample}
+
+  context "when a weather query is made" do 
+    it "returns the weather" do 
+      expect(context).to be_a_success
+      expect(context.weather[:lat]).to be_truthy
+      expect(context.weather[:lng]).to be_truthy
+      expect(context.weather[:sunrise]).to be_truthy
+      expect(context.weather[:sunset]).to be_truthy
+    end 
+  end 
 end
--- spec/interactors/signup_user_spec.rb
+++ spec/interactors/signup_user_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe SignupUser, type: :interactor do
-  describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+  let (:email) {Faker::Internet.email}
+  let (:password) {Faker::Internet.password(min_length: 10)}
+  let (:city) {Faker::Hipster.word}
+
+  subject(:context) do
+    described_class.call(email: email, password: password, city: city)
+  end
+
+  context "when a valid email is provided" do
+    it "should allow a user signup" do
+      expect(context).to be_a_success
+      expect(context.user.id).to be_truthy
+      expect(context.token).to be_truthy
+    end
+  end
+
+  context "when an existing email is provided" do
+    let!(:user) do 
+      User.create!(email: email, password: password, city: city) 
+    end
+
+    it "should return an error" do 
+      expect(context).to be_a_failure
+      puts "#{context.inspect}"
+    end 
   end
 end
