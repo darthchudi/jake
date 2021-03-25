3f9cbf28754be66814c10f71d7813860bbd092bf
darthchudi <chukwudioranu@ymail.com>
2021-03-25 11:06:38 +0100

    feat: update signup user mutation test

--- spec/graphql/mutations/signup_user_spec.rb
+++ spec/graphql/mutations/signup_user_spec.rb
@@ -6,15 +6,30 @@ module Mutations
         let (:password) {Faker::Internet.password(min_length: 10)}
         let (:city) {Faker::Hipster.word}
 
-        it "should allow a user signup" do 
-            post :execute, params: {query: query}
+        context "when a valid email is provided" do
+            it "should allow a user signup" do 
+                post :execute, params: {query: query}
+                result = JSON.parse(response.body)
 
-            result = JSON.parse(response.body)
+                expect(result["data"]["signup"]["user"]["id"]).to be_truthy 
+                expect(result["data"]["signup"]["token"]).to be_truthy 
+            end
+        end 
 
-            expect(result["data"]["signup"]["user"]["id"]).to be_truthy 
-            expect(result["data"]["signup"]["token"]).to be_truthy 
-        end
+        context "when an existing email is provided" do
+            let!(:user) do 
+                User.create!(email: email, password: password, city: city) 
+            end 
 
+            it "should return an error" do 
+                post :execute, params: {query: query}
+                result = JSON.parse(response.body)
+
+                expect(result["data"]["signup"]).to eq nil
+                expect(result["errors"][0]["message"]).to eq "Validation failed: Email has already been taken"
+                puts "#{result.inspect}"
+            end 
+        end 
 
         def query
             <<~GQL
